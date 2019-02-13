fb0d260f226db1f106692d2c1c1ff0cbfbd88b26
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-12 20:12:42 -0700

    Adding open and close function to top nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// JS goes here
+const hamburgerMenu = document.querySelector(".hamburger");
+const topNav = document.querySelector(".open-nav");
+const header = document.querySelector("header");
+
+
+hamburgerMenu.addEventListener("click", _ => {
+    topNav.classList.toggle("show-nav");
+    topNav.classList.toggle("hide-nav");
+    header.classList.toggle("show-nav");
+});
