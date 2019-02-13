05b46ebe26a94c6fa90e0a370af41b45c5c9bb11
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-12 21:23:56 -0700

    fixing fonts

--- js/index.js
+++ js/index.js
@@ -7,4 +7,9 @@ hamburgerMenu.addEventListener("click", _ => {
     topNav.classList.toggle("show-nav");
     topNav.classList.toggle("hide-nav");
     header.classList.toggle("show-nav");
+    hamburgerMenu.classList.toggle("close-burger");
 });
+
+// This is to avoid selecting text when clicking the menu button open and close
+// quickly
+hamburgerMenu.addEventListener("dblclick", e => e.preventDefault());
