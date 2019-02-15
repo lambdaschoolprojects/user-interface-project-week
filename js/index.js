300998183c0d63d82d1e3ea6eecc9fd4ca4d2bb7
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-14 21:31:28 -0700

    more navigation refactor

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const header = document.querySelector("header");
 hamburgerMenu.addEventListener("click", _ => {
     topNav.classList.toggle("show-nav");
     topNav.classList.toggle("hide-nav");
-    header.classList.toggle("show-nav");
+    //header.classList.toggle("show-nav");
     hamburgerMenu.classList.toggle("close-burger");
 });
 
